af30c76a3ed04e2461f255f891e595167496690b
Ademir Júnior <adejr0220@gmail.com>
2024-10-05 10:02:47 -0300

    :sparkles: new component Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import { Home } from "./components/Home";
+
 function App() {
   return (
     <>
-      <main>
-        <h1>Seja bem vindo! :)</h1>
-        <p>Desafio de apresentação Pessoal do Projeto Frontend fusion</p>
-      </main>
+      <Home />
     </>
   );
 }
