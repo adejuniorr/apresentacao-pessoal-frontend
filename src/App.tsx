b3f7c2579d301f44301d20ce18661b8cf55d84f2
Ademir Júnior <adejr0220@gmail.com>
2024-10-05 19:41:41 -0300

    :sparkles: new component Projects.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { AboutMe } from "./components/AboutMe";
 import { Home } from "./components/Home";
+import { Projects } from "./components/Projects";
 
 function App() {
   return (
-    <div className="text-slate-50 px-4 py-6 w-[90vw] lg:w-[70vw] xl:w-[50vw] m-auto">
+    <div className="text-slate-50 px-4 py-6 md:py-0 w-[90vw] lg:w-[70vw] xl:w-[50vw] m-auto">
       <Home />
       <AboutMe />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,10 @@
+export const Projects = () => {
+  return (
+    <div
+      id="projects"
+      className="h-screen flex items-center justify-center text-2xl"
+    >
+      #Projects
+    </div>
+  );
+};
