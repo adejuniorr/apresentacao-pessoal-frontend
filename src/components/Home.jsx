61d0b69fd566cf170797e9eca1aea1b7fb709f66
Ademir Júnior <adejr0220@gmail.com>
2024-10-05 10:13:57 -0300

    :arrow_up: update Home.jsx
    Add presentation text, picture and social media links.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,28 @@
+import me from "/me-dark.svg";
+
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main>
+      <div>
+        <h1>Oi, eu sou o <span>Adê</span> =)</h1>
+        <h2>E sou <span>Desenvolvedor Front-End</span></h2>
+        <p>
+          Aqui nesta página de apresentação você pode conhecer um pouco mais{" "}
+          <a href="#about-me">sobre mim</a> e também conhecer alguns dos{" "}
+          <a href="#projects">meus projetos</a>.
+        </p>
+        <div>
+          <p>Entre em contato ;)</p>
+          <div>
+            <a href="https://whatsa.me/5577991379957">whatsapp</a> {" "}
+            <a href="https://github.com/adejuniorr/">github</a> {" "}
+            <a href="https://www.linkedin.com/in/dev-ademirjunior/">linkedin</a>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src={me} alt="Foto do desenvolvedor" />
+      </div>
+    </main>
+  );
+};
