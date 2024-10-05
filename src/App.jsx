7f5b44ef73eaccf7b9936207a83fd4e77a7c1a9d
Ademir Júnior <adejr0220@gmail.com>
2024-10-05 10:56:47 -0300

    :sparkles: new component AboutMe.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { AboutMe } from "./components/AboutMe";
 import { Home } from "./components/Home";
 
 function App() {
   return (
     <div className="h-screen bg-slate-900 text-slate-50">
       <Home />
+      <AboutMe />
     </div>
   );
 }
