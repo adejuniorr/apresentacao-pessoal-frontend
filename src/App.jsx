0e10982a561e2f58e5152247ee2f030f0cff135c
Ademir Júnior <adejr0220@gmail.com>
2024-10-05 10:18:58 -0300

    :lipstick: style App.jsx
    Set background color (slate-900 - #0F172A) and text color (slate-50 - #F8FAFC)

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { Home } from "./components/Home";
 
 function App() {
   return (
-    <>
+    <div className="h-screen bg-slate-900 text-slate-50">
       <Home />
-    </>
+    </div>
   );
 }
 
